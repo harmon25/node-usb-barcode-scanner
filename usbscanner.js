d4710ccdc8e8eec56576823432a0d3ee06b17d7d
harmoN <dougwright1@gmail.com>
2015-09-20 20:17:27 -0400

    update keymap
    missed symbols and letters from Code39 barcodes

--- usbscanner.js
+++ usbscanner.js
@@ -28,10 +28,16 @@ usbScanner.prototype.init = function(options){
 	var allDevices = getDevices();
 	//hidMap defining keyboard code to coresponding string value
 	this.hidMap = options.hidMap || {
-    30: '1', 31: '2', 32: '3', 33: '4',34: '5',
-    35: '6', 36: '7', 37: '8', 38: '9',39: '0',
-    // 'e' or enter? - barcode escape char
-    40: 'e'
+	4:"A", 5:"B",6:"C",7:"D",8:"E",
+	9:"F", 10:"G", 11:"H",12:"I",13:"J",
+	14:"K",15:"L",16:"M",17:"N", 18:"O",
+	19:"P",20:"Q",21:"R",22:"S",23:"T",
+	24:"U",25:"V", 26:"W", 27:"X", 28:"Y",
+	29:"Z",30: '1', 31: '2', 32: '3', 33: '4',
+	34: '5',35: '6', 36: '7', 37: '8', 38: '9',
+	// enter - barcode escape char
+	39: '0',40: 'enter', 44:" ",45:"-", 55:".", 56:"/",
+	85:"*", 87:"+"
 	};
 
 	var scanner = _.find(allDevices, function(device) {
